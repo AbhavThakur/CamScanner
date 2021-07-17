0c10a95928c46e118cb81d0b7c6ee18e5d44d497
Abhav <thakur.abhav25@gmail.com>
2021-07-17 15:17:50 +0530

    camer and gallery

--- app/screens/Camera.js
+++ app/screens/Camera.js
@@ -7,6 +7,8 @@ import {
   TouchableOpacity,
   Platform,
   TextInput,
+  Pressable,
+  KeyboardAvoidingView,
 } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 import {
@@ -16,24 +18,19 @@ import {
   Card,
   Title,
   Paragraph,
-  Modal,
   Button,
+  Divider,
 } from 'react-native-paper';
 import CameraRoll from '@react-native-community/cameraroll';
+import Modal from 'react-native-modal';
 
 function Camera(props) {
   const [image, setImage] = useState(null);
-  const [visible, setVisible] = React.useState(false);
+  const [txt, settxt] = useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
 
-  const showModal = () => setVisible(true);
-  const hideModal = () => setVisible(false);
-  const containerStyle = {
-    backgroundColor: '#fff',
-    padding: 20,
-    height: '50%',
-    width: '100%',
-    position: 'absolute',
-    bottom: 0,
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
   };
 
   const takePhotoFromCamera = () => {
@@ -54,43 +51,59 @@ function Camera(props) {
   };
 
   const savePhoto = () => {
-    // CameraRoll.save(image, {type: 'photo', album: 'cameraScanner'})
-    //   .then(() => alert('Document Saved in gallery'))
-    //     .catch(err => alert('Image not save', err));
     const uri = image;
 
     const filename = uri.substring(uri.lastIndexOf('/') + 1);
     const uploadUri = Platform.OS === 'ios' ? uri.replace('file:///', '') : uri;
     console.log('filename', filename);
     console.log('uploadUri', uploadUri);
+
+    CameraRoll.save(image, {type: 'photo', album: txt})
+      .then(() => alert('Document Saved in gallery'))
+      .catch(err => alert('Image not save', err));
   };
   return (
     <View style={styles.container}>
+      <Button style={styles.btn} mode="contained" onPress={takePhotoFromCamera}>
+        Open camera
+      </Button>
       {image === null ? null : (
         <Image source={{uri: image}} style={{width: '100%', height: '70%'}} />
       )}
 
-      <Button
-        style={{marginVertical: 20}}
-        mode="contained"
-        onPress={takePhotoFromCamera}>
-        Open camera
+      <Button style={styles.btn} mode="contained" onPress={toggleModal}>
+        Save Document
       </Button>
-      <Button mode="contained" onPress={showModal}>
-        Save photo
-      </Button>
-      <Provider>
-        <Portal>
-          <Modal
-            visible={visible}
-            onDismiss={hideModal}
-            contentContainerStyle={containerStyle}>
-            <Text style={{color: '#000'}}>
-              Example Modal. Click outside this area to dismiss.
-            </Text>
-          </Modal>
-        </Portal>
-      </Provider>
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Save Document in Gallery</Text>
+            <TextInput
+              placeholder="Enter the file name"
+              value={txt}
+              onChangeText={text => settxt(text)}
+            />
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <View style={{flex: 1, height: 1, backgroundColor: '#ccc'}} />
+            </View>
+            <Text>{txt}</Text>
+
+            <Pressable
+              style={[
+                styles.button,
+                {backgroundColor: '#2196F3', width: '70%', marginTop: 10},
+              ]}
+              onPress={savePhoto}>
+              <Text style={styles.textStyle}>Save Document in Gallery</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={toggleModal}>
+              <Text style={styles.textStyle}>close </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -98,6 +111,58 @@ function Camera(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20,
+    height: '50%',
+    width: '90%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+    width: '50%',
+    //   marginVertical: 20,
+    position: 'absolute',
+    bottom: 20,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  btn: {
+    marginVertical: 20,
+    width: 200,
   },
 });
 
